d9dd0eb09019fd2c2317fb1ae31cec351aa3c628
Chris Bradford <87334834+bradford583@users.noreply.github.com>
2021-09-02 20:45:14 -0600

    can now search for books

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var userFormEl = document.querySelector("#book");
+var searchBtn = document.querySelector(".search");
+
+var bookResult = userFormEl.value;
+console.log(bookResult);
+
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+
+  var bookResult = userFormEl.value.trim();
+  console.log(bookResult);
+    fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:" + bookResult + "&maxResults=20&printType=books&orderBy=relavance")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
+
+searchBtn.addEventListener("click", formSubmitHandler);
