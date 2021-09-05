d4f5325b3b622e1bbad6d03e836740590a2a1a1b
Tyler <tylersundquist3@gmail.com>
2021-09-05 13:12:07 -0600

    search working for both functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,17 @@ var searchBtn = document.querySelector(".search");
 var bookResult = userFormEl.value;
 console.log(bookResult);
 
-// var formSubmitHandler = function (event) {
-//   event.preventDefault();
+var formSubmitHandler = function (event) {
+  event.preventDefault();
 
-//   var bookResult = userFormEl.value.trim();
-//   console.log(bookResult);
-//     fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:" + bookResult + "&maxResults=20&printType=books&orderBy=relevance")
-//     .then((response) => response.json())
-//     .then((data) => console.log(data));
-// };
+  var bookResult = userFormEl.value.trim();
+  console.log(bookResult);
+    fetch("https://www.googleapis.com/books/v1/volumes?q=intitle:" + bookResult + "&maxResults=20&printType=books&orderBy=relevance")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+};
 
-// searchBtn.addEventListener("click", formSubmitHandler);
+searchBtn.addEventListener("click", formSubmitHandler);
 
 const apiKey = "rOks7u7aABNkDxOUutMyH0ZGf3ixyGWm";
 
@@ -38,5 +38,3 @@ searchBtn.addEventListener("click", formSubmitHandler);
 // https://api.nytimes.com/svc/books/v3/reviews.json?api-key=
 
 // rOks7u7aABNkDxOUutMyH0ZGf3ixyGWm
-
-// AyMlAvgR8kDM9SOMERKkD0wyBCc1Hr1q
